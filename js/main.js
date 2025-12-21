131313ea4fec2426b76363303a1007fc07ad67af
Luis Viteri <108193534+viteripro@users.noreply.github.com>
2025-12-21 18:52:45 -0500

    Actualizado
    Coloque funciones para que cuando seleccione un item de la lista filtre segun su categoria

--- js/main.js
+++ js/main.js
@@ -198,35 +198,69 @@ const productosArray = [
     }
 ];
 
-
 const contenedorProductos = document.querySelector("#contenedor-productos");
-const botonesCategorias = document.querySelectorAll(".boton-categoria")
+const botonesCategorias = document.querySelectorAll(".boton-categoria");
+const tituloPrincipal = document.querySelector("#titulo-principal");
+let botonesAgregar = document.querySelectorAll(".producto-agregar");
 
 
-function cargarProductos(){
-   productos.forEach(producto => {
+function cargarProductos(productosElegidos){
 
-     const div = document.createElement("div");
-     div.classList.add("producto");
-     div.innerHTML = `
-      <img class="producto-imagen" src="${producto.imagen}" alt="${producto.titulo}">
-      <div class="producto-detalles">
-        <h3 class="producto-titulo">${producto.titulo}</h3>
-        <p class="producto-precio">${producto.precio}</p>
-        <button class="producto-agregar" id="${producto.id}" >Agregar</button>
-                   </div>
-     `
-     contenedorProductos.append(div);
-   })   
+   contenedorProductos.innerHTML ="";
+
+   productosElegidos.forEach(producto=>{
+  
+    const div = document.createElement("div");
+    div.classList.add("producto");
+    div.innerHTML= `
+    <img class="producto-imagen" src="${producto.imagen}" alt="${producto.titulo}" >
+    <div class="producto-detalles">
+    <h3 class="producto-titulo">${producto.titulo}</h3>
+    <p class="producto-precio">${producto.precio}</p>
+    <button class="producto-agregar" id="${producto.id}">Agregar</button>
+    </div>    
+    `;
+    contenedorProductos.append(div);
+ })
+
+ actualizarBotonesAgregar();
 }
 
- cargarProductos();
+cargarProductos(productos);
+  
+botonesCategorias.forEach(boton =>{
+    boton.addEventListener("click",(e)=>{
+      
+    botonesCategorias.forEach(boton =>boton.classList.remove("active"));
+     e.currentTarget.classList.add("active");
+       
+    if(e.currentTarget.id != "todos"){           
+     const productoCategoria = productos.find(producto => producto.categoria.id === e.currentTarget.id);
+     tituloPrincipal.innerText = productoCategoria.categoria.nombre;
+    const productosBoton = productos.filter(producto => producto.categoria.id === e.currentTarget.id);
+     cargarProductos(productosBoton);   
+        }else{
+            tituloPrincipal.innerText = "Todos los productos";
+            cargarProductos(productos);
 
- botonesCategorias.forEach(boton => {
-    boton.addEventListener("click", (e) => {
+        }
 
-        botonesCategorias.forEach(boton => boton.classList.remove("active"))
 
-        e.currentTarget.classList.add("active"); 
     })
- })
+ });
+
+ function actualizarBotonesAgregar(){
+   botonesAgregar = document.querySelectorAll(".producto-agregar");  
+   
+   botonesAgregar.forEach(boton => {
+     boton.addEventListener("click",agregarAlCarrito)
+   });
+ }
+
+const productosEnCarrito = [];
+
+function agregarAlCarrito(e) {
+  const idBoton = e.currentTarget.id;
+  const productoAgregado = productos.find(producto => producto.id === idBoton);
+  console.log(productoAgregado);   
+}
