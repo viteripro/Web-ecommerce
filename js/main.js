24a6f125c5ab24f3a34a1cb6503b3a10c7b4fe23
Luis Viteri <108193534+viteripro@users.noreply.github.com>
2025-12-26 19:08:27 -0500

    Colocando funciones
    Tratando de que se acumule en un array la cantidad de productos que estan seleccionados

--- js/main.js
+++ js/main.js
@@ -202,6 +202,8 @@ const contenedorProductos = document.querySelector("#contenedor-productos");
 const botonesCategorias = document.querySelectorAll(".boton-categoria");
 const tituloPrincipal = document.querySelector("#titulo-principal");
 let botonesAgregar = document.querySelectorAll(".producto-agregar");
+const numerito = document.querySelector("#numerito");
+
 
 
 function cargarProductos(productosElegidos){
@@ -262,5 +264,23 @@ const productosEnCarrito = [];
 function agregarAlCarrito(e) {
   const idBoton = e.currentTarget.id;
   const productoAgregado = productos.find(producto => producto.id === idBoton);
-  console.log(productoAgregado);   
+
+
+   if(productosEnCarrito.some(producto => producto.id === idBoton)){
+    const index = productosEnCarrito.findIndex(producto => producto.id === idBoton);
+    productosEnCarrito[index].cantidad++;  
+  }else{
+    productoAgregado.cantidad = 1;
+    productosEnCarrito.push(productoAgregado);
+  }
+  
+  actualizarNumerito();
+
+  localStorage.setItem("productos-en-carrito",JSON.stringify(productosEnCarrito));
+
+}
+
+function actualizarNumerito(){
+ let nuevoNumerito = productosEnCarrito.reduce((acc,producto) => acc + producto.cantidad, 0);
+ numerito.innerText = nuevoNumerito;
 }
